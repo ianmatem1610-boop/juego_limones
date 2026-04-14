1d3d087b737eeb0f5e188a598c0a5686ba7a05e4
ianm1610 <ianmetem1610@gmail.com>
2026-04-14 16:42:00 -0500

    agregar dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,2 +1,23 @@
-let canvas = document.getElementById ("areaJuego");
+let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+
+const ALTURA_SUELO = 20
+const ALTURA_PERSONAJE = 60
+const ANCHO_PERSONAJE = 40
+
+
+function iniciar(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo(){
+    ctx.fillStyle = "blue"; 
+    ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle = "yellow"; 
+    ctx.fillRect(canvas.width / 2 - 20, canvas.height -(ALTURA_SUELO+ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE); 
+}
