6d1ebfdb66855de54d8c0b03d12009b1f54ca54f
ianm1610 <ianmetem1610@gmail.com>
2026-04-17 23:08:33 -0500

    agregando las funciones de vidas y puntajes

--- limones.js
+++ limones.js
@@ -5,13 +5,15 @@ const ALTURA_SUELO = 20;
 const ALTURA_PERSONAJE = 60;
 const ANCHO_PERSONAJE = 40;
 const ANCHO_LIMON = 20;
-const ALTO_LIMON =20;
+const ALTURA_LIMON =20;
 
 
 let  personajeX = canvas.width/2;
 let  personajeY = canvas.height -(ALTURA_SUELO+ALTURA_PERSONAJE)
 let limonX= canvas.width/2;
 let limonY=0;
+let puntaje=0;
+let vidas=3;
 
 
 function iniciar(){
@@ -56,24 +58,36 @@ function limpiarCanva(){
 
 function dibujarLimon(){
     ctx.fillStyle = "green"; 
-    ctx.fillRect(limonX,limonY, ANCHO_LIMON, ALTO_LIMON);
+    ctx.fillRect(limonX,limonY, ANCHO_LIMON, ALTURA_LIMON);
 }
 
 function  bajarLimon(){
     limonY = limonY +10;
     actualizarPantalla();
-    detectarColicion();
+    detectarAtrapado();
+    detectarPiso();
 }
 
-function detectarColicion (){
+function detectarAtrapado (){
     if (limonX + ANCHO_LIMON>personajeX && 
         limonX < personajeX+ANCHO_PERSONAJE &&
-        limonY + ALTO_LIMON>personajeY && 
+        limonY + ALTURA_LIMON>personajeY && 
         limonY < personajeY+ALTURA_PERSONAJE){
-
-        //alert("ATRAPADO")
+        //alert(ATRAPADO!!!)
         aparacerLimon();
+        puntaje = puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
     }
+}
+
+function detectarPiso(){
+if(limonY+ALTURA_LIMON==canvas.height-ALTURA_SUELO){
+    aparacerLimon();
+    vidas=vidas-1;
+    mostrarEnSpan("txtVidas",vidas);
+
+}
+
 }
 
 function generarAleatorio(min,max){
--- utilis.js
+++ utilis.js
@@ -1,8 +1,14 @@
 function generarAleatorio(min,max){
     let random=Math.random();
     //ejemplo: max es 600 , min es 5
-    let numero=random(max-min);//0-max 
+    let numero=random*(max-min);//0-max 
     let numeroEntero = parseInt(numero);
      numeroEntero = numeroEntero+min;
     return numeroEntero;
+}
+
+
+function mostrarEnSpan(idSpan,valor){
+    let componente = document.getElementById(idSpan);
+    componente.textContent=valor;
 }
